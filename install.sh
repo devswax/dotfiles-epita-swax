2e83bd9a0a02185ab67e6b554a7a225cc7719681
Swax <axel.rauger@epita.fr>
2025-03-12 16:54:32 +0100

    fix wofi + add bashrc, starship, gtk3/4

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults"
+
+for f in $dot_list; do
+  rm -rf "$HOME/.$f"
+  ln -s "$AFS_DIR/.confs/$f" "$HOME/.$f"
+done
+
+
+while IFS= read -r line; do
+    echo "installing $line"
+    nix profile install "nixpkgs#$line"
+done < packages.txt
+nix profile install --impure --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }'
+
+
+CURSOR_PATH=~/.local/share/icons
+if ! test -d $CURSOR_PATH/rose; then
+    echo "Installing custom cursor..."
+    mkdir -p $CURSOR_PATH
+    git clone git@github.com:ndom91/rose-pine-hyprcursor.git $CURSOR_PATH/rose
+fi
