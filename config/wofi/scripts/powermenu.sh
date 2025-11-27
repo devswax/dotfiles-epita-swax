de421edfdef38956937d94ab23eba19a21a32f4d
devswax <axel@viccosta.eu>
2025-11-27 13:21:57 +0100

    fix little bug in powermenu.sh

--- config/wofi/scripts/powermenu.sh
+++ config/wofi/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-entries="⇠ Logout\n⊗ Lock\n⟲ Reboot\n⏻ Shutdown"
+entries="⇠ Logout\n Lock\n⟲ Reboot\n⏻ Shutdown"
 
 selected=$(echo -e $entries|wofi --width 250 --height 210 --xoffset=1660 --yoffset=0 --prompt=leaving? --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
